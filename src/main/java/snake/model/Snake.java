538b2d7a3eb2da18d27d8e726f8b0c073a6178bc
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-09-29 14:22:21 +0200

    implemented class Snake

--- src/main/java/snake/model/Snake.java
+++ src/main/java/snake/model/Snake.java
@@ -1,4 +1,27 @@
 package snake.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Snake {
+    private List<SnakePart> snake = new ArrayList<>();
+
+    public Snake() {
+        //default snake with three parts
+        addPart(0,0);
+        addPart(0,1);
+        addPart(0,2);
+    }
+
+    public List<SnakePart> getSnake() {
+        return snake;
+    }
+
+    public SnakePart getPart(int i){
+        return snake.get(i);
+    }
+
+    public void addPart(int x, int y){
+        snake.add(new SnakePart(x, y));
+    }
 }
