ff66ce09b7dcc254a48759c7638a35fbd2455b16
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-09-29 15:02:14 +0200

    implemented class Food

--- src/main/java/snake/model/Food.java
+++ src/main/java/snake/model/Food.java
@@ -1,7 +1,45 @@
 package snake.model;
 
+import java.util.Random;
+
 public class Food {
     private int x;
     private int y;
-    private Colors color;
+    private Colors color = Colors.BLUE;
+
+    public Food(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Colors getColor() {
+        return color;
+    }
+
+    public void setColor(Colors color) {
+        this.color = color;
+    }
+
+    public void nextColor(){
+        Random random = new Random();
+        int color = random.nextInt(5);
+
+        setColor(Colors.values()[color]);
+    }
 }
