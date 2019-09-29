71c53f076152e10b3dff868996931fff9751c355
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-09-29 14:01:00 +0200

    initialized the board controller

--- src/main/java/snake/controllers/BoardController.java
+++ src/main/java/snake/controllers/BoardController.java
@@ -1,4 +1,23 @@
 package snake.controllers;
 
-public class BoardController {
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BoardController implements Initializable {
+    public Canvas canvas;
+    public Button buttonUp;
+    public Button buttonRight;
+    public Button buttonLeft;
+    public Button buttonDown;
+    public Label score;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
