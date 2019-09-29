b0110fd8520a1d71c6520d40398b558e7af47ac9
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-09-29 15:30:15 +0200

    implemented main game timer in game controller class

--- src/main/java/snake/controllers/BoardController.java
+++ src/main/java/snake/controllers/BoardController.java
@@ -1,7 +1,9 @@
 package snake.controllers;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -16,8 +18,32 @@ public class BoardController implements Initializable {
     public Button buttonDown;
     public Label score;
 
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+
+        new AnimationTimer(){
+            long lastTick = 0;
+
+            @Override
+            public void handle(long now) {
+                if(lastTick == 0){
+                    lastTick = now;
+                    play(graphicsContext);
+                    return;
+                }
+
+                /*if(now - lastTick > 1000000000 / speed){
+                    lastTick = now;
+                    play(graphicsContext);
+                }*/
+            }
+        }.start();
+    }
+
+    private void play(GraphicsContext graphicsContext) {
 
     }
 }
