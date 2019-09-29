37348ea0883085651bfd1dd04b7beb7f5bffd328
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-09-29 14:07:28 +0200

    SnakePart class implementation

--- src/main/java/snake/model/SnakePart.java
+++ src/main/java/snake/model/SnakePart.java
@@ -1,4 +1,38 @@
 package snake.model;
 
+import java.util.Objects;
+
 public class SnakePart {
+    private int x;
+    private int y;
+
+    public SnakePart(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void movePart(int deltaX, int deltaY){
+        setX(getX() + deltaX);
+        setY(getY() + deltaY);
+    }
+
+    public boolean inCollision(int x, int y){
+        return getX() == x && getY() == y;
+    }
 }
