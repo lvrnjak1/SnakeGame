b13f76f287ec4e8beaf420bf057555d760193820
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-09-29 14:31:41 +0200

    implemented enum Direction

--- src/main/java/snake/model/Direction.java
+++ src/main/java/snake/model/Direction.java
@@ -1,4 +1,21 @@
 package snake.model;
 
 public enum Direction {
+    UP(-1, 0), LEFT(0,-1), RIGHT(0, 1), DOWN(1,0);
+
+    private int deltaX;
+    private int deltaY;
+
+    Direction(int deltaX, int deltaY){
+        this.deltaX = deltaX;
+        this.deltaY = deltaY;
+    }
+
+    public int getDeltaX(){
+        return deltaX;
+    }
+
+    public int getDeltaY(){
+        return deltaY;
+    }
 }
