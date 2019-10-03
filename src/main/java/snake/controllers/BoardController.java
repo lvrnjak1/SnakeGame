447369619a4b8d0ea56701842f240d6dd3c6b920
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-10-03 07:43:05 +0200

    method that draws the starting position of the snake and the food

--- src/main/java/snake/controllers/BoardController.java
+++ src/main/java/snake/controllers/BoardController.java
@@ -1,50 +1,51 @@
 package snake.controllers;
 
-import javafx.animation.AnimationTimer;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import snake.model.Game;
+import snake.model.Snake;
+import snake.model.SnakePart;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BoardController implements Initializable {
     public Canvas canvas;
-    public Button buttonUp;
-    public Button buttonRight;
-    public Button buttonLeft;
-    public Button buttonDown;
+    public Button pauseButton; //when it is clicked change text to resume and vice versa
+    public Button restartButton;
+    public Button endGameButton;
     public Label score;
+    public BorderPane pane;
 
     private Game game;
+
+    private static final int width = 40;
+    private static final int heigth = 40;
+    private static final int pixel = 10;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         game = new Game();
         GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+        drawBoard(graphicsContext);
+    }
 
-        new AnimationTimer(){
-            long lastTick = 0;
+    private void drawBoard(GraphicsContext graphicsContext) {
+        graphicsContext.fillRect(0,0,width * pixel,heigth * pixel);
 
-            @Override
-            public void handle(long now) {
-                if(lastTick == 0){
-                    lastTick = now;
-                    play(graphicsContext);
-                    return;
-                }
+        Snake snake = game.getSnake();
+        for(SnakePart part : snake.getSnake()){
+            graphicsContext.setFill(Color.WHITE);
+            graphicsContext.fillRect(part.getX() * pixel, part.getY() * pixel, pixel, pixel);
+        }
 
-                if(now - lastTick > 1000000000 / game.getSnake().getSpeed()){
-                    lastTick = now;
-                    play(graphicsContext);
-                }
-            }
-        }.start();
-
-
+        graphicsContext.setFill(Color.RED);
+        graphicsContext.fillRect(game.getFood().getX() * pixel, game.getFood().getY() * pixel, pixel, pixel);
     }
 
     private void play(GraphicsContext graphicsContext) {
