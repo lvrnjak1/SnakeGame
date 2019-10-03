324c04befc89603a5ed71d946917ebc7de023f82
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-10-03 15:12:46 +0200

    added more colors for food

--- src/main/java/snake/utility/Colors.java
+++ src/main/java/snake/utility/Colors.java
@@ -3,7 +3,10 @@ package snake.utility;
 import javafx.scene.paint.Color;
 
 public enum Colors {
-    BLUE(Color.BLUE), GREEN(Color.GREEN), PINK(Color.PINK), ORANGE(Color.ORANGE), YELLOW(Color.YELLOW);
+    BLUE(Color.BLUE), GREEN(Color.GREEN), PINK(Color.PINK), ORANGE(Color.ORANGE), YELLOW(Color.YELLOW),
+    MAGENTA(Color.CRIMSON), LIGHT_BLUE(Color.LIGHTBLUE), LIME(Color.LIME), PURPLE(Color.PURPLE), PEACH(Color.PEACHPUFF),
+    GRAY(Color.GRAY), LIGHT_GREEN(Color.LIGHTGREEN), CORAL(Color.CORAL), RED(Color.RED), TEAL(Color.TEAL),
+    BROWN(Color.CHOCOLATE), VIOLET(Color.VIOLET);
 
     private javafx.scene.paint.Paint color;
 
