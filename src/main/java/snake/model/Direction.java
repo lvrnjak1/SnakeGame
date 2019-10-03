1c7e8167022badcfe3fe72bc734d96f5105d0538
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-10-03 08:17:24 +0200

    changed deltax and deltay to match the real coordinate system

--- src/main/java/snake/model/Direction.java
+++ src/main/java/snake/model/Direction.java
@@ -1,7 +1,7 @@
 package snake.model;
 
 public enum Direction {
-    UP(-1, 0), LEFT(0,-1), RIGHT(0, 1), DOWN(1,0);
+    UP(0, -1), LEFT(-1,0), RIGHT(1, 0), DOWN(0,1);
 
     private int deltaX;
     private int deltaY;
