1429ef72f7117f5fb793df0852f4e04220de9175
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-10-03 07:58:13 +0200

    added a paused property to the game class

--- src/main/java/snake/model/Game.java
+++ src/main/java/snake/model/Game.java
@@ -6,12 +6,12 @@ public class Game {
     private Snake snake;
     private Food food;
 
-    private static final int boardWidth = 38;
-    private static final int boardHeight = 38;
-    private static final int squareSize = 10;
+    private static final int boardWidth = 40;
+    private static final int boardHeight = 40;
 
     private int score;
     private boolean gameOver;
+    private boolean paused;
 
     public Game() {
         int randX = (new Random()).nextInt(boardHeight);
@@ -21,6 +21,7 @@ public class Game {
         this.food = new Food(randX, randY);
         this.score = 0;
         this.gameOver = false;
+        this.paused = false;
     }
 
     public Snake getSnake() {
@@ -39,15 +40,19 @@ public class Game {
         return boardHeight;
     }
 
-    public static int getSquareSize() {
-        return squareSize;
-    }
-
     public int getScore() {
         return score;
     }
 
     public boolean isGameOver() {
         return gameOver;
+    }
+
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+    }
+
+    public boolean getPaused(){
+        return paused;
     }
 }
