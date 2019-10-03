473bc253c0bbfe009a52d253bae6c1b86cd5db74
lvrnjak1 <lvrnjak1@etf.unsa.ba>
2019-10-03 13:25:18 +0200

    stage title

--- src/main/java/snake/game/Main.java
+++ src/main/java/snake/game/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/board.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Snake Game");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
